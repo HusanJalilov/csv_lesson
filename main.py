721fffa44b8923db502b566b019d9f39c69ffad0
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-01-22 18:45:45 +0500

    Task

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 def get_column(data):
     column_name = [data]
+    column_name=column_name[0]
+    column_name=column_name[:31].split(",")
+    
     return column_name
 
 data = open('data.csv').read()
