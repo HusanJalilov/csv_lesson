da62dc4a6a0ffecc931be5bb90d34d3d2c4f50b3
Hu$an <76938934+HusanJalilov@users.noreply.github.com>
2022-01-22 19:04:31 +0500

    Task

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 
 def get_column(data):
-    column_name = [data].split("/n")
-    column_name=column_name[].split(",")
+    
+    column_name=data.split()
     
     
-    return column_name
+    
+    return column_name[0].split(",")
 
 data = open('data.csv').read()
 print(get_column(data))
